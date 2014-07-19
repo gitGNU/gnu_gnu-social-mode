5b7d171392db4f2a2a0713f3ecba9bdf602f64ba
Sergio Durigan Junior <sergiodj@sergiodj.net>
2014-07-18 21:17:34 -0300

    Relicense code to GPLv3+ for gnu-social-mode
    This is the first commit of the new gnu-social-mode project.  It simply
    relicenses all the code to GPLv3+ on all files.  Very tiny cleanups were
    also made on the top comments of those files as well.
    
    Welcome, gnu-social-mode!

--- bbdb-identica.el
+++ bbdb-identica.el
@@ -1,18 +1,22 @@
-;;; bbdb-identica.el ---
+;;; bbdb-identica.el --- Integrate BBDB and gnu-social-mode
+
+;; This file is part of gnu-social-mode.
 ;;
-;; Filename: bbdb-identica.el
-;; Description:
-;; Author: Christian
-;; Maintainer:
-;; Created: dom oct  2 22:15:13 2011 (-0300)
-;; Version:
-;; Last-Updated:
-;;           By:
-;;     Update #: 0
-;; URL:
-;; Keywords:
-;; Compatibility:
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
 ;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
 ;; Features that might be required by this library:
 ;;
 ;;   BBDB
@@ -34,23 +38,6 @@
 ;;
 ;;; Change Log:
 ;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
--- identica-friends.el
+++ identica-friends.el
@@ -1,7 +1,25 @@
-;;; identica-friends.el ---
+;;; identica-friends.el --- Check who are your friends on GNU Social
+
+;; This file is part of gnu-social-mode
 ;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
 ;; Filename: identica-friends.el
-;; Description: A library that provides some functions to look who are your friends in your identi.ca account.
+;; Description: A library that provides some functions to look who are 
+;;              your friends in your identi.ca account.
 ;; Author: Christian Giménez
 ;; Maintainer:
 ;; Created: dom sep 25 17:58:40 2011 (-0300)
@@ -41,24 +59,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Change Log:
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
